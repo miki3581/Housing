44ccad0152d4d8b686a4cf59294b80f3a91788e8
Mikołaj Kadłubowski <mikolaj_kadlubowski@wp.pl>
2026-07-24 16:16:43 +0200

    CV and modified main

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from data_loader import load_data
 from preprocess import clean_data, engineer_features, encode_features
 from model import split_data, scale_data, train_linear_regression, evaluate_model, save_model, load_model
 from visualize import plot_actual_vs_predicted, plot_residuals, plot_residuals_vs_predicted
+from model_xgb import optimize_params
 
 def main():
     # Set to 'warszawa', 'szczecin', 'gdansk', etc. or None for the whole dataset
     TARGET_CITY = 'warszawa'  
+    model = 'XGB'
     
     # Loading data
     df = load_data()
@@ -26,39 +28,45 @@ def main():
     # Encoding categorical features
     df_encoded = encode_features(df_engineered)
 
-    # Splitting data into train and test sets
-    X_train, X_test, y_train, y_test = split_data(df_encoded, target_col='price')
-    
-    # Scaling numerical features
-    X_train_scaled, X_test_scaled, scaler = scale_data(X_train, X_test)
+    if model == 'Lin_reg':
 
-    # Training Linear Regression model
-    model = train_linear_regression(X_train_scaled, y_train)
+        # Splitting data into train and test sets
+        X_train, X_test, y_train, y_test = split_data(df_encoded, target_col='price')
+        
+        # Scaling numerical features
+        X_train_scaled, X_test_scaled, scaler = scale_data(X_train, X_test)
 
-    # Saving the model and the scaler as a dictionary
-    model_filename = "housing_model.joblib"
-    artifacts_to_save = {'model': model, 'scaler': scaler}
-    save_model(artifacts_to_save, model_filename)
-    
-    # Loading the model and scaler (to demonstrate it works without retraining)
-    loaded_artifacts = load_model(model_filename)
-    loaded_model = loaded_artifacts['model']
-    loaded_scaler = loaded_artifacts['scaler'] # This is ready to transform new data!
+        # Training Linear Regression model
+        model = train_linear_regression(X_train_scaled, y_train)
 
-    # Evaluating model performance
-    metrics = evaluate_model(loaded_model, X_test_scaled, y_test)
-    print("\nModel Performance on Test Set:")
-    print(f"Mean Absolute Error (MAE): {metrics['MAE']:.2f} PLN")
-    print(f"Root Mean Squared Error (RMSE): {metrics['RMSE']:.2f} PLN")
-    print(f"R-squared (R2): {metrics['R2']:.4f}")
-    
-    # Visualisation
-    y_pred = loaded_model.predict(X_test_scaled)
-    plot_actual_vs_predicted(y_test, y_pred, TARGET_CITY)
-    
-    plot_residuals(y_test, y_pred, TARGET_CITY)
+        # Saving the model and the scaler as a dictionary
+        model_filename = "housing_model.joblib"
+        artifacts_to_save = {'model': model, 'scaler': scaler}
+        save_model(artifacts_to_save, model_filename)
+        
+        # Loading the model and scaler (to demonstrate it works without retraining)
+        loaded_artifacts = load_model(model_filename)
+        loaded_model = loaded_artifacts['model']
+        loaded_scaler = loaded_artifacts['scaler'] # This is ready to transform new data!
 
-    plot_residuals_vs_predicted(y_test, y_pred, TARGET_CITY)
+        # Evaluating model performance
+        metrics = evaluate_model(loaded_model, X_test_scaled, y_test)
+        print("\nModel Performance on Test Set:")
+        print(f"Mean Absolute Error (MAE): {metrics['MAE']:.2f} PLN")
+        print(f"Root Mean Squared Error (RMSE): {metrics['RMSE']:.2f} PLN")
+        print(f"R-squared (R2): {metrics['R2']:.4f}")
+        
+        # Visualisation
+        y_pred = loaded_model.predict(X_test_scaled)
+        plot_actual_vs_predicted(y_test, y_pred, TARGET_CITY)
+        
+        plot_residuals(y_test, y_pred, TARGET_CITY)
+
+        plot_residuals_vs_predicted(y_test, y_pred, TARGET_CITY)
+
+    elif model == 'XGB':
+        best_model, best_params, cv_results = optimize_params(df_encoded)
+
 
 if __name__ == "__main__":
     main()
--- model_xgb.py
+++ model_xgb.py
@@ -2,16 +2,67 @@ import pandas as pd
 import numpy as np
 from xgboost import XGBRegressor
 from scipy.stats import randint, uniform, loguniform
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-param_grid = {
-    'n_estimators': randint(100, 500),
-    'max_depth': randint(3, 10),
-    'learning_rate': loguniform(0.001, 0.3),
-    'subsample': uniform(0.7, 0.3),
-    'colsample_bytree': uniform(0.7, 0.3),
-    'gamma': loguniform[0.001, 1],
-    'reg_alpha': loguniform[0, 100],
-    'reg_lambda': loguniform[0, 1000]
-}
 
-xgb_model = XGBRegressor(random_state=42)
+def optimize_params(
+        df: pd.DataFrame, 
+        target_col: str = 'price', 
+        param_grid: dict = None, 
+        search_type: str = 'random', 
+        n_splits: int = 5,
+        random_state: int = 42,
+        scoring: str = 'neg_mean_squared_error',
+        n_jobs: int = -1,
+        n_iter: int = 50,
+    ) -> tuple:
+
+    X = df.drop(columns=[target_col])
+    y = df[target_col]
+    
+    if param_grid is None:
+        param_grid = {
+            'n_estimators': randint(100, 500),
+            'max_depth': randint(3, 10),
+            'learning_rate': loguniform(0.001, 0.3),
+            'subsample': uniform(0.7, 0.3),
+            'colsample_bytree': uniform(0.7, 0.3),
+            'gamma': loguniform(0.001, 1000),
+            'reg_alpha': loguniform(0.001, 10),
+            'reg_lambda': loguniform(0.1, 50)
+        }
+
+    xgb_model = XGBRegressor(random_state=42)
+
+    if search_type == 'grid':
+        cv_search = GridSearchCV(
+            estimator = xgb_model, 
+            param_grid = param_grid, 
+            scoring=scoring, 
+            n_jobs=n_jobs, 
+            cv=n_splits,
+            verbose = 1
+            )
+    elif search_type == 'random':
+        cv_search = RandomizedSearchCV(
+            estimator=xgb_model, 
+            param_distributions=param_grid, 
+            n_iter=n_iter, 
+            cv=n_splits, 
+            scoring=scoring, 
+            n_jobs=n_jobs, 
+            verbose = 1, 
+            random_state=random_state)
+
+    cv_search.fit(X, y)
+
+    best_model = cv_search.best_estimator_
+    best_params = cv_search.best_params_
+
+    print(f"\nBest CV MAE: {-cv_search.best_score_:.3f}")
+    print({"Best parameters:"})
+    for i, j in best_params.items():
+        print(f"{i}: {j}")
+
+    return best_model, best_params, pd.DataFrame(cv_search.cv_results_)
